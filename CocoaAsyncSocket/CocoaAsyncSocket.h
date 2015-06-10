a85d47650b089166fdfc12eadab320350e983444
Kra Larivain <olarivain@gmail.com>
2015-06-09 18:15:33 -0700

    added carthage project

---
+++ CocoaAsyncSocket/CocoaAsyncSocket.h
@@ -0,0 +1,23 @@
+//
+//  CocoaAsyncSocket.h
+//  CocoaAsyncSocket
+//
+//  Created by Olivier Larivain on 6/9/15.
+//  Copyright (c) 2015 CocoaAsyncSocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaAsyncSocket.
+FOUNDATION_EXPORT double CocoaAsyncSocketVersionNumber;
+
+//! Project version string for CocoaAsyncSocket.
+FOUNDATION_EXPORT const unsigned char CocoaAsyncSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaAsyncSocket/PublicHeader.h>
+
+
+#import <CocoaAsyncSocket/GCDAsyncSocket.h>
+#import <CocoaAsyncSocket/GCDAsyncUdpSocket.h>
+#import <CocoaAsyncSocket/AsyncUdpSocket.h>
+#import <CocoaAsyncSocket/AsyncSocket.h>
